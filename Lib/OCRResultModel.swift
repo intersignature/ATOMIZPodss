90568da9b5fa8e03f223f5354aaa7192b4b6eb62
Sirichai <drink@finnomena.com>
2021-06-22 03:42:51 +0700

    Create OCRResultModel.swift

---
+++ Lib/OCRResultModel.swift
@@ -0,0 +1,15 @@
+//  
+//  OCRResultModel.swift
+//  NterApp
+//
+//  Created by Drink Sirichai on 11/5/2564 BE.
+//
+
+import Foundation
+
+struct OCRResultModel {
+
+    let uuid: String?
+    let imageURL: URL?
+
+}
